ab31dd190e7c081d7aa28f234b773abd6e6b07b8
Thuan <nguyenquangthuan2906@gmail.com>
2022-03-10 16:55:47 +0700

    change height of item in carousel, add section OCR, change image when down size width

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import {
   ButtonUpload,
+  ContentCard,
+  ContentCardText,
   ContentImage,
   HistoryContainer,
 } from "../pages/home/homeStyle";
@@ -127,6 +129,19 @@ const History = () => {
             return (
               <ContentModalContainer key={"key" + count}>
                 <ContentImage src={value.toString()} />
+                {/* For OCR */}
+                {/* <ContentCard className="contentCard" isResult={true}>
+                  <ContentCardText copyable={true}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum
+                  </ContentCardText>
+                </ContentCard> */}
                 <ContentModaText>{name[count]}</ContentModaText>
                 <ContentModaText>
                   {created[count].split(" ")[1].split(".")[0].toString() +
--- src/components/inOutput.tsx
+++ src/components/inOutput.tsx
@@ -8,24 +8,57 @@ import {
 } from "../pages/home/homeStyle";
 
 interface IInputOutput {
-  image: string;
+  imageInput?: string;
+  output?: string;
   position: string;
 }
 
-const InputOutput = ({ image = "", position }: IInputOutput) => {
+const InputOutput = ({
+  imageInput = "",
+  output = "",
+  position,
+}: IInputOutput) => {
   return (
     <ContentSideContainer>
-      {image ? (
-        <ContentImage src={image} />
+      {/* For Predicted */}
+      {imageInput ? (
+        <ContentImage className="contentImage" src={imageInput} />
       ) : position === "Input" ? (
-        <ContentCard>
+        <ContentCard className="contentCard">
           <ContentCardText>No Image Uploaded</ContentCardText>
         </ContentCard>
       ) : (
-        <ContentCard>
+        <ContentCard className="contentCard">
           <ContentCardText>No Image Predicted</ContentCardText>
         </ContentCard>
       )}
+
+      {/* For OCR */}
+      {/* {position === "Input" ? (
+        imageInput ? (
+          <ContentImage src={imageInput} />
+        ) : (
+          <ContentCard className="contentCard">
+            <ContentCardText>No Image Uploaded</ContentCardText>
+          </ContentCard>
+        )
+      ) : output ? (
+        <ContentCard className="contentCard" isResult={true}>
+          <ContentCardText copyable={true}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </ContentCardText>
+        </ContentCard>
+      ) : (
+        <ContentCard className="contentCard">
+          <ContentCardText>No Image Predicted</ContentCardText>
+        </ContentCard>
+      )} */}
       <ContentPosition>{position}</ContentPosition>
     </ContentSideContainer>
   );
