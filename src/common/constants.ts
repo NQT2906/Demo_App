fbd7f8b5909e1290b0f3bcc1abd146aa0cd0f71a
Thuan <nguyenquangthuan2906@gmail.com>
2022-06-15 01:57:45 +0700

    change base url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export const SERVER_URL =
-  "http://ec2-3-145-105-42.us-east-2.compute.amazonaws.com:3000/thuan" ||
+  "http://ec2-3-134-101-6.us-east-2.compute.amazonaws.com:3000/thuan" ||
   process.env.REACT_APP_SERVER_URL;
 export const SWIPE_TIME = 3200;
 export const ANIMATION_TIME = 800;
